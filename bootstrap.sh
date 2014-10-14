6cd19f7daaa0169aa14e77808b75a61c4aed0497
Stefan van Gastel <stefanvangastel@gmail.com>
2014-10-14 15:56:08 +0200

    Updated to Geoserver 2.6 war file
    Changed to most recent GeoServer version 2.6 and using direct link instead.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ apt-get -y install zip openjdk-7-jre tomcat7
 if [ -e geoserver.zip ]
 then
 echo "fetching geoserver.war"
-wget -q -O geoserver.zip http://sourceforge.net/projects/geoserver/files/GeoServer/2.3.5/geoserver-2.3.5-war.zip
+wget -q -O geoserver.zip http://downloads.sourceforge.net/project/geoserver/GeoServer/2.6.0/geoserver-2.6.0-war.zip?r=&ts=1413294840&use_mirror=garr
 fi
 if [ -e /var/lib/tomcat7/webapps/geoserver.war ]
 then
