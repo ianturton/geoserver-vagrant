884145c7d44d3e2134dd5696cba1e86815b7f9a2
Ian Turton <ijturton@gmail.com>
2016-04-06 10:46:09 +0100

    added css module

---
+++ cookbooks/geoserver/recipes/add_css.rb
@@ -0,0 +1,45 @@
+# fetch the CSS extension and addit to the running geoserver and restart
+#
+# Ian Turton
+#
+
+include_recipe 'java'
+include_recipe 'tomcat::default'
+include_recipe 'geoserver'
+
+package "unzip"
+package "curl"
+
+if "#{node[:geoserver][:version]}" == "latest" 
+  url = "http://ares.boundlessgeo.com/geoserver/master/ext-latest/geoserver-2.9-SNAPSHOT-css-plugin.zip"
+elsif node[:geoserver][:version].include? "x" 
+  #http://ares.boundlessgeo.com/geoserver/2.8.x/ext-latest/geoserver-2.8-SNAPSHOT-css-plugin.zip
+  basename = node[:geoserver][:version]
+  url = "http://ares.boundlessgeo.com/geoserver/#{node[:geoserver][:version]}/ext-latest/geoserver-#{basename[0..-3]}-SNAPSHOT-css-plugin.zip"
+else
+url = "http://sourceforge.net/projects/geoserver/files/GeoServer/#{node['geoserver']['version']}/extensions/geoserver-#{node['geoserver']['version']}-css-plugin.zip"
+end
+
+plugin_name = ::File.basename(url)
+plugin_local_path = ::File.join(Chef::Config[:file_cache_path],plugin_name)
+tomcat_directory = "#{node['tomcat']['webapp_dir']}/geoserver/WEB-INF/lib"
+
+remote_file "#{plugin_local_path}" do
+    source url
+    notifies :run, 'execute[install plugin]', :immediately
+    action :create_if_missing
+end
+
+execute 'install plugin' do
+    user #{node['tomcat']['user']}
+    unpack = <<-EOF
+      cd #{tomcat_directory}
+      unzip -u #{plugin_local_path} 
+    EOF
+    command unpack
+end
+
+
+service 'tomcat6' do
+  action :restart
+end
